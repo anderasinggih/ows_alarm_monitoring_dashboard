0ffafdfd7a8efead1d53318a17aa2d64083ad984
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-08-07 14:22:38 +0700

    fix: use SELECT * for ap_alarm_live as streaming push models require full projection to resolve live alarms in OWS

--- src/service/alarmGet.js
+++ src/service/alarmGet.js
@@ -364,7 +364,7 @@ var siteInClause = fwaSiteCodesList.length > 0 ? (" AND sitecode IN (" + fwaSite
 // ==========================================
 // 4. QUERY LIVE & HISTORY ALARMS
 // ==========================================
-var liveTql = "SELECT sitecode, site_code, site_id, siteid, sitename, sitedownfault, domain, firstinserttime, firstoccurrence, eventtime, event_time, cleartime, alarmname, alarm_name, rawalarmname FROM \"/AlarmBase/ICT_AlarmPush/ap_alarm_live\" WHERE (sitedownfault = '1' OR sitedownfault = 1)" + siteInClause;
+var liveTql = "SELECT * FROM \"/AlarmBase/ICT_AlarmPush/ap_alarm_live\" WHERE (sitedownfault = '1' OR sitedownfault = 1)" + siteInClause;
 
 var historyTql = "SELECT sitecode, site_code, site_id, siteid, sitename, sitedownfault, domain, firstinserttime, firstoccurrence, eventtime, event_time, cleartime, alarmname, alarm_name, rawalarmname FROM \"/AlarmBase/ICT_History_Query/ict_hq_es_history\" " +
     "WHERE firstinserttime <= " + windowEndMs + " " +
