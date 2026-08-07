b22c478014a4746c00ee2c09e696e13532935046
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-08-07 14:20:37 +0700

    perf: select complete explicit alarm fields list (including fallback aliases) without using SELECT *

--- src/service/alarmGet.js
+++ src/service/alarmGet.js
@@ -364,9 +364,9 @@ var siteInClause = fwaSiteCodesList.length > 0 ? (" AND sitecode IN (" + fwaSite
 // ==========================================
 // 4. QUERY LIVE & HISTORY ALARMS
 // ==========================================
-var liveTql = "SELECT * FROM \"/AlarmBase/ICT_AlarmPush/ap_alarm_live\" WHERE (sitedownfault = '1' OR sitedownfault = 1)" + siteInClause;
+var liveTql = "SELECT sitecode, site_code, site_id, siteid, sitename, sitedownfault, domain, firstinserttime, firstoccurrence, eventtime, event_time, cleartime, alarmname, alarm_name, rawalarmname FROM \"/AlarmBase/ICT_AlarmPush/ap_alarm_live\" WHERE (sitedownfault = '1' OR sitedownfault = 1)" + siteInClause;
 
-var historyTql = "SELECT * FROM \"/AlarmBase/ICT_History_Query/ict_hq_es_history\" " +
+var historyTql = "SELECT sitecode, site_code, site_id, siteid, sitename, sitedownfault, domain, firstinserttime, firstoccurrence, eventtime, event_time, cleartime, alarmname, alarm_name, rawalarmname FROM \"/AlarmBase/ICT_History_Query/ict_hq_es_history\" " +
     "WHERE firstinserttime <= " + windowEndMs + " " +
     "AND (cleartime >= " + windowStartMs + " OR cleartime = 0 OR cleartime IS NULL) " +
     "AND (sitedownfault = '1' OR sitedownfault = 1)" + siteInClause;
